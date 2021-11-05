9a0a62574bbb5815362c21e3d6f63309b8f00255
Elson Pinheiro <elson.silva@ufopa.edu.br>
2021-11-05 01:24:55 -0300

    Project created

---
+++ backend/src/test/java/com/epdelivery/epdelivery/EpdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epdelivery.epdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
