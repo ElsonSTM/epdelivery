d6cf2b23cf4cf9279e4f7e68576c77b24883b282
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-01-11 20:14:07 -0300

    Add routes and StepsHeader

--- front-web/src/Navbar/Index.tsx
+++ front-web/src/Navbar/Index.tsx
@@ -1,11 +1,12 @@
 import './styles.css';
 import { ReactComponent as Logo} from './logo.svg';
+import { Link } from 'react-router-dom';
 
 function Navbar(){
   return (
     <nav className="main-navbar">
       <Logo />
-      <a href="home" className="logo-text">EP Delivery</a>
+      <Link to="/" className="logo-text">EP Delivery</Link>
     </nav>
   )
 }
---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,23 @@
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Navbar from './Navbar/Index';
+import Orders from "./Order";
+
+function Routes(){
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Switch>
+        <Route path="/orders">
+          <Orders />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes;
