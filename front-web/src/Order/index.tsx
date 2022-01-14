0055734cc69af464f8717267c388f0bf887e3ed1
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-01-14 01:11:55 -0300

    Pesquisa do local do pedido

--- front-web/src/Order/index.tsx
+++ front-web/src/Order/index.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import OrdersLocation from './OrderLocation';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
+import { OrderLocationData, Product } from './types';
 
 function Orders(){
+  const [products, setProducts] = useState<Product[]>([]);
+  const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+  useEffect(() => {
+    fetchProducts()
+    .then(response => setProducts(response.data))
+    .catch(error => console.log(error))
+  }, []);
+
+  console.log(products);
+
   return (
     <div className="order-container">
       <StepsHeader />
+      <ProductsList products={products} />
+      <OrdersLocation onChangeLocation={location => setOrderLocation(location)} />
     </div>
   )
 }
